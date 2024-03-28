2f26674dc1047089c29841f4c0ad7d0b7ab9cbce
nicolas <nico.cardenas.l@outlook.com>
2024-03-28 13:45:34 +0100

    Added selecting fractal functionality

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg,
     #NavigationToolbar2Tk
 )
-from data_process import generateIFS
+from subcode.data_process import generateIFS
 
 file_path = 'sierpinski.txt'
+available_params = ['sierpinski','leaf','barnsley']
 
 class Fenetre(tk.Tk):
     def __init__(self):
@@ -27,6 +28,7 @@ class Fenetre(tk.Tk):
         #create tk widgets
         self.draw_widgets()
 
+
     def draw_scatter_top(self, x_points:list, y_points:list, col:str='white'):
         figure = plt.Figure(figsize=(6, 4), dpi=100)
         
@@ -40,15 +42,30 @@ class Fenetre(tk.Tk):
 
         figure.add_axes(ax)
         
-        ax.scatter(x_points, y_points, color=col, s=0.5, linewidths=0)
-        
+        scatter = ax.scatter(0,0)
         ax.set_axis_off()
 
         figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-        return figure, ax
+        return figure, ax # ,scatter
+    
+    def replot(self,event):
+        print("replotting")
+        self.ax.clear()
+        x, y = generateIFS(available_params[self.entry.curselection()[0]]+".txt",10000)
+        self.ax.scatter(x, y, color='white', s=0.5, linewidths=0)
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
 
     def draw_widgets(self):
+        self.params = tk.Variable(value=available_params)
+        
+        self.entry = tk.Listbox(self, listvariable=self.params,height=6,selectmode=tk.SINGLE)
+        self.btn_replot = tk.Button(self,text="generate")
+
+        self.btn_replot.bind("<Button-1>",func=self.replot)
+        self.btn_replot.pack(side=tk.RIGHT)
+        self.entry.pack(side=tk.LEFT)
         self.scale = tk.Scale(orient='horizontal')
         self.scale.pack()
 
