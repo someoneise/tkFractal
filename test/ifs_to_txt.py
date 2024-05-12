b731855c9a95836679ceace028fbb997cbe7149a
nicolas <nico.cardenas.l@outlook.com>
2024-05-13 00:39:17 +0200

    chore: PEP8 linting

--- test/ifs_to_txt.py
+++ test/ifs_to_txt.py
@@ -12,11 +12,11 @@ flag = True
 print(files)
 for file_name in files:
     file_path = os.path.join(folder_path, file_name)
-    
+
     # Check if the path is a file
     if os.path.isfile(file_path):
-        with open(file_path, 'r',newline='') as file:
-            with open(file_path[:-4]+'.txt','w') as newfile:
+        with open(file_path, 'r', newline='') as file:
+            with open(file_path[:-4]+'.txt', 'w') as newfile:
                 file.__next__()
                 while True:
                     line = file.readline().strip()
@@ -26,7 +26,3 @@ for file_name in files:
                     newfile.write(line+'\n')
             newfile.close()
         file.close()
-
-
-
-            
